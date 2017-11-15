df7f37b0247648af11ac630e701b898434684b66
jaov <jesalfochvel@gmail.com>
2017-11-15 12:47:56 -0400

    Subida inicial de clases

---
+++ Proyecto Final Jesus Ochoa/src/Objetos/Objeto.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+import java.awt.Rectangle;
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author jesus.ochoa
+ */
+public class Objeto extends JLabel{
+
+    private int x;
+    private int y,hp,orientacion;
+    boolean isAlive;
+
+    public Objeto(int x, int y, Icon image, int tipo) {
+        super(image);
+        this.x = x;
+        this.orientacion=0;
+        this.y = y;
+        this.hp=Resources.Datos.vectorhp[tipo];
+
+        //0 avion, 1 barco, 2 gasolina, 3 misil, 4 pared
+        
+        
+    }
+    public Rectangle getRectangulo(){
+        return new Rectangle(x, y, this.getWidth(), this.getHeight());
+    }
+    
+    public void cuandoColisiona(boolean esAliado){
+        if (!esAliado) {
+            this.hp--;
+        }    
+        
+        if (hp<=0) {
+            this.isAlive=false;
+        }
+    }
+    
+    public void moverse(){
+        this.x+=orientacion;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setOrientacion(int orientacion) {
+        this.orientacion *= orientacion;
+    }
+
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+    
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+    public void movY(int paso){
+        this.y+=paso;
+    }
+    public void movX(int paso){
+        this.x+=paso*this.orientacion;
+    }
+    
+    
+}
---
+++ Proyecto Final Jesus Ochoa/src/Resources/Datos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resources;
+
+/**
+ *
+ * @author jesus.ochoa
+ */
+public interface Datos {
+    //0 avion, 1 barco, 2 gasolina, 3 misil, 4 pared
+    public static final int[] vectorhp = {3,2,1,1,1};
+    public static final int dano =1;
+}
